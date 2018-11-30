0cc6996b0cfc44f335143be771ec64df1cb70dda
Gabriela Fabricia de Oliveira <gabriela.fabricia.de.oliveira@ibm.com>
2018-11-30 09:13:53 -0200

    Primeiro Commit

---
+++ src/main/java/com/ibm/publkafka/PublKafkaApplication.java
@@ -0,0 +1,36 @@
+package com.ibm.publkafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PublKafkaApplication {
+
+	@Autowired
+	private KafkaTemplate<String, Object> template;
+
+	private String topic = "helloww";
+
+	@GetMapping("/publish/{name}")
+	public String publishMessage(@PathVariable String name) {
+		template.send(topic, "Hi " + name + " Welcome to Chilli's");
+		return "Data published";
+	}
+
+	@GetMapping("/publishJson")
+	public String publishMessage() {
+		User user = new User(1234, "Userrrr", new String[] { "Brasil", "BRBR", "Av Brasil" });
+		template.send(topic, user);
+		return "Json Data published";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublKafkaApplication.class, args);
+	}
+}
